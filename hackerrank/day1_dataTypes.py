aa11722f80198bb620f080b4daaeedd9e9f1996b
mmarseal <mpurwosyahputra@gmail.com>
2025-07-24 00:11:30 +0700

    Day 1: Data Types

---
+++ hackerrank/day1_dataTypes.py
@@ -0,0 +1,17 @@
+""" input user:
+12
+4.0
+is the best place to learn and practice coding!
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+a = int(input())
+b = float(input())
+c = (input())
+
+print(i + a)
+print(d + b)
+print(s + c)
