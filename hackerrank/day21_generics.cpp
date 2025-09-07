17cb46c3b614140d2c4c816050cea7cf06c9ebf7
mmarseal <mpurwosyahputra@gmail.com>
2025-09-07 22:09:41 +0700

    Day 21: Generics

---
+++ hackerrank/day21_generics.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+void printArray(vector<T> arr){
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << endl;
+    }    
+}
+
+int main() {
+	int n;
+	
+	cin >> n;
+	vector<int> int_vector(n);
+	for (int i = 0; i < n; i++) {
+		int value;
+		cin >> value;
+		int_vector[i] = value;
+	}
+	
+	cin >> n;
+	vector<string> string_vector(n);
+	for (int i = 0; i < n; i++) {
+		string value;
+		cin >> value;
+		string_vector[i] = value;
+	}
+
+	printArray<int>(int_vector);
+	printArray<string>(string_vector);
+
+	return 0;
+}
+
+// Input :
+// 3
+// 1
+// 2
+// 3
+// 2
+// Hello
+
+// Expected Output:
+// 1
+// 2
+// 3
+// Hello
+
